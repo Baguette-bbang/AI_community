9d26f62519f9f48e828c4b49765ff389d2fef936
차민재 <91722759+new-coder-g@users.noreply.github.com>
2025-05-12 19:32:27 +0900

    Delete app/modules/posts directory

--- app/modules/posts/views.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-from app.modules.posts.controllers import index
-
-posts_bp = Blueprint('posts', __name__, url_prefix='/posts')
-
-# 전체 글 관련 라우트
-posts_bp.route('/')(index)
